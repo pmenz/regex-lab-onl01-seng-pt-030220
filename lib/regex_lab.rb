6c640b6161200bffade32b39844c76d3311922bd
Pablo Menz <p.menz.r@gmail.com>
2020-03-16 04:54:12 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,14 +1,15 @@
 def starts_with_a_vowel?(word)
-if word[0].scan(/[aeiou]/) == "a"||"e"||"i"||"o"||"u" then 
+ if word[0].scan(/[aeiou]/) == "a"||"e"||"i"||"o"||"u" then 
   true 
-else 
+ else 
   false 
 end
  
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+array=[]
+if word
 end
 
 def words_five_letters_long(text)
