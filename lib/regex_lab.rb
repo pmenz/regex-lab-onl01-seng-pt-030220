d002443fb14ff31b43341deba9daa3fa86a3f263
Pablo Menz <p.menz.r@gmail.com>
2020-03-16 04:32:18 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
- word.scan(/[aeiou]/){|vowel| if vowel == "a" then true end }
+ word.scan(/[aeiou]/){|vowel| if vowel == "a"||"e"||"i"||"o"||"u" then true end }
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
