6a8f0ffb989b0942daeb567ff36da4eb0176bf3f
Pablo Menz <p.menz.r@gmail.com>
2020-03-16 04:25:34 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
- word.scan(/[aeiou]/){if |vowel|== "a" || "e"|| "i"||"o"||"u"}
+ word.scan(/[aeiou]/){|vowel|=a
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
